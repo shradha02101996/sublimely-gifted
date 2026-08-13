4ba99e266fab717a655110586b646e4e614c2765
shradha02101996 <biyanishradha21@gmail.com>
2026-08-13 21:18:21 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='nightly-thanks-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;})));});
